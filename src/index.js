affad801ff07c3ae58ef63cf66b8ff4a11bb9b21
Max Han <mwellh84@gmail.com>
2020-04-17 13:50:44 -0400

    Update configs

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Hello World</div>
+      </div>
+    );
+  }
+}
+export default MyComponent;
